7b5aef278e78a619149ed5fbe18a23ea0d47eeb8
romeros69 <jefops@gmail.com>
2023-02-22 22:53:08 +0300

    feat: add new command help

--- internal/commands/help.go
+++ internal/commands/help.go
@@ -1,11 +1,17 @@
 package commands
 
+import "fmt"
+
 type Help struct {
 }
 
 // Do - TODO
 func (h *Help) Do() error {
-	panic("panic")
+	fmt.Printf("Usage: genos [options] <arguments>\n\n" +
+		"Options:\n" +
+		"\t-h, --help\t\tList of options\n" +
+		"\t-i, --init-layout\tGenerate project layout\n")
+	return nil
 }
 
 func (h *Help) GetNames() []string {
