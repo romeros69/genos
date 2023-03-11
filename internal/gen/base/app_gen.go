01d4804d4aa90d8b4834f7d8852d559d6b57d274
romeros69 <jefops@gmail.com>
2023-03-11 12:50:52 +0300

    feat: add base generation app and main

--- internal/gen/base/app_gen.go
+++ internal/gen/base/app_gen.go
@@ -8,36 +8,444 @@ import (
 	"os"
 )
 
-func createAppAST() *ast.File {
+func createAppAST(moduleName string) *ast.File {
 	return &ast.File{
 		Name: ast.NewIdent("app"),
 		Decls: []ast.Decl{
-			&ast.GenDecl{
+			0: &ast.GenDecl{
 				Tok: token.IMPORT,
 				Specs: []ast.Spec{
-					&ast.ImportSpec{
+					0: &ast.ImportSpec{
 						Path: &ast.BasicLit{
 							Kind:  token.STRING,
-							Value: "\"fmt\"",
+							Value: "\"log\"",
+						},
+					},
+					1: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"os\"",
+						},
+					},
+					2: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"os/signal\"",
+						},
+					},
+					3: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"" + moduleName + "/configs\"",
+						},
+					},
+					4: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"" + moduleName + "/pkg/httpserver\"",
+						},
+					},
+					5: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"" + moduleName + "/pkg/postgres\"",
+						},
+					},
+					6: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"syscall\"",
+						},
+					},
+					7: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"time\"",
+						},
+					},
+					8: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"github.com/gin-contrib/cors\"",
+						},
+					},
+					9: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"github.com/gin-gonic/gin\"",
 						},
 					},
 				},
 			},
-			&ast.FuncDecl{
+			1: &ast.FuncDecl{
 				Name: ast.NewIdent("Run"),
-				Type: &ast.FuncType{},
+				Type: &ast.FuncType{
+					Params: &ast.FieldList{
+						List: []*ast.Field{
+							{
+								Names: []*ast.Ident{
+									ast.NewIdent("cfg"),
+								},
+								Type: &ast.StarExpr{
+									X: &ast.SelectorExpr{
+										X:   ast.NewIdent("configs"),
+										Sel: ast.NewIdent("Config"),
+									},
+								},
+							},
+						},
+					},
+				},
 				Body: &ast.BlockStmt{
 					List: []ast.Stmt{
-						&ast.ExprStmt{
+						0: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								0: ast.NewIdent("_"),
+								1: ast.NewIdent("err"),
+							},
+							Tok: token.DEFINE,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: &ast.SelectorExpr{
+										X:   ast.NewIdent("postgres"),
+										Sel: ast.NewIdent("New"),
+									},
+									Args: []ast.Expr{
+										ast.NewIdent("cfg"),
+									},
+								},
+							},
+						},
+						1: &ast.IfStmt{
+							Cond: &ast.BinaryExpr{
+								X:  ast.NewIdent("err"),
+								Op: token.NEQ,
+								Y:  ast.NewIdent("nil"),
+							},
+							Body: &ast.BlockStmt{
+								List: []ast.Stmt{
+									&ast.ExprStmt{
+										X: &ast.CallExpr{
+											Fun: &ast.SelectorExpr{
+												X:   ast.NewIdent("log"),
+												Sel: ast.NewIdent("Fatal"),
+											},
+											Args: []ast.Expr{
+												&ast.BasicLit{
+													Kind:  token.STRING,
+													Value: "\"Error in creating postgres instance\"",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						2: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								ast.NewIdent("handler"),
+							},
+							Tok: token.DEFINE,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: &ast.SelectorExpr{
+										X:   ast.NewIdent("gin"),
+										Sel: ast.NewIdent("New"),
+									},
+								},
+							},
+						},
+						3: &ast.ExprStmt{
 							X: &ast.CallExpr{
 								Fun: &ast.SelectorExpr{
-									X:   ast.NewIdent("fmt"),
-									Sel: ast.NewIdent("Println"),
+									X:   ast.NewIdent("handler"),
+									Sel: ast.NewIdent("Use"),
 								},
 								Args: []ast.Expr{
-									&ast.BasicLit{
-										Kind:  token.STRING,
-										Value: "\"Run complete!\"",
+									&ast.CallExpr{
+										Fun: &ast.SelectorExpr{
+											X:   ast.NewIdent("cors"),
+											Sel: ast.NewIdent("New"),
+										},
+										Args: []ast.Expr{
+											&ast.CompositeLit{
+												Type: &ast.SelectorExpr{
+													X:   ast.NewIdent("cors"),
+													Sel: ast.NewIdent("Config"),
+												},
+												Elts: []ast.Expr{
+													0: &ast.KeyValueExpr{
+														Key: ast.NewIdent("AllowOrigins"),
+														Value: &ast.CompositeLit{
+															Type: &ast.ArrayType{
+																Elt: ast.NewIdent("string"),
+															},
+															Elts: []ast.Expr{
+																&ast.BasicLit{
+																	Kind:  token.STRING,
+																	Value: "\"*\"",
+																},
+															},
+														},
+													},
+													1: &ast.KeyValueExpr{
+														Key: ast.NewIdent("AllowMethods"),
+														Value: &ast.CompositeLit{
+															Type: &ast.ArrayType{
+																Elt: ast.NewIdent("string"),
+															},
+															Elts: []ast.Expr{
+																&ast.BasicLit{
+																	Kind:  token.STRING,
+																	Value: "\"*\"",
+																},
+															},
+														},
+													},
+													2: &ast.KeyValueExpr{
+														Key: ast.NewIdent("AllowHeaders"),
+														Value: &ast.CompositeLit{
+															Type: &ast.ArrayType{
+																Elt: ast.NewIdent("string"),
+															},
+															Elts: []ast.Expr{
+																0: &ast.BasicLit{
+																	Kind:  token.STRING,
+																	Value: "\"Access-Control-Allow-Origin\"",
+																},
+																1: &ast.BasicLit{
+																	Kind:  token.STRING,
+																	Value: "\"*\"",
+																},
+															},
+														},
+													},
+													3: &ast.KeyValueExpr{
+														Key: ast.NewIdent("ExposeHeaders"),
+														Value: &ast.CompositeLit{
+															Type: &ast.ArrayType{
+																Elt: ast.NewIdent("string"),
+															},
+															Elts: []ast.Expr{
+																&ast.BasicLit{
+																	Kind:  token.STRING,
+																	Value: "\"Content-Length\"",
+																},
+															},
+														},
+													},
+													4: &ast.KeyValueExpr{
+														Key:   ast.NewIdent("AllowCredentials"),
+														Value: ast.NewIdent("true"),
+													},
+													5: &ast.KeyValueExpr{
+														Key: ast.NewIdent("MaxAge"),
+														Value: &ast.BinaryExpr{
+															X: &ast.BasicLit{
+																Kind:  token.INT,
+																Value: "12",
+															},
+															Op: token.MUL,
+															Y: &ast.SelectorExpr{
+																X:   ast.NewIdent("time"),
+																Sel: ast.NewIdent("Hour"),
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						4: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								ast.NewIdent("serv"),
+							},
+							Tok: token.DEFINE,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: &ast.SelectorExpr{
+										X:   ast.NewIdent("httpserver"),
+										Sel: ast.NewIdent("New"),
+									},
+									Args: []ast.Expr{
+										0: ast.NewIdent("handler"),
+										1: &ast.CallExpr{
+											Fun: &ast.SelectorExpr{
+												X:   ast.NewIdent("httpserver"),
+												Sel: ast.NewIdent("Port"),
+											},
+											Args: []ast.Expr{
+												&ast.SelectorExpr{
+													X:   ast.NewIdent("cfg"),
+													Sel: ast.NewIdent("AppPort"),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						5: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								ast.NewIdent("interruption"),
+							},
+							Tok: token.DEFINE,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: ast.NewIdent("make"),
+									Args: []ast.Expr{
+										0: &ast.ChanType{
+											Dir: 3,
+											Value: &ast.SelectorExpr{
+												X:   ast.NewIdent("os"),
+												Sel: ast.NewIdent("Signal"),
+											},
+										},
+										1: &ast.BasicLit{
+											Kind:  token.INT,
+											Value: "1",
+										},
+									},
+								},
+							},
+						},
+						6: &ast.ExprStmt{
+							X: &ast.CallExpr{
+								Fun: &ast.SelectorExpr{
+									X:   ast.NewIdent("signal"),
+									Sel: ast.NewIdent("Notify"),
+								},
+								Args: []ast.Expr{
+									0: ast.NewIdent("interruption"),
+									1: &ast.SelectorExpr{
+										X:   ast.NewIdent("os"),
+										Sel: ast.NewIdent("Interrupt"),
+									},
+									2: &ast.SelectorExpr{
+										X:   ast.NewIdent("syscall"),
+										Sel: ast.NewIdent("SIGTERM"),
+									},
+								},
+							},
+						},
+						7: &ast.SelectStmt{
+							Body: &ast.BlockStmt{
+								List: []ast.Stmt{
+									0: &ast.CommClause{
+										Comm: &ast.AssignStmt{
+											Lhs: []ast.Expr{
+												ast.NewIdent("s"),
+											},
+											Tok: token.DEFINE,
+											Rhs: []ast.Expr{
+												&ast.UnaryExpr{
+													Op: token.ARROW,
+													X:  ast.NewIdent("interruption"),
+												},
+											},
+										},
+										Body: []ast.Stmt{
+											&ast.ExprStmt{
+												X: &ast.CallExpr{
+													Fun: &ast.SelectorExpr{
+														X:   ast.NewIdent("log"),
+														Sel: ast.NewIdent("Printf"),
+													},
+													Args: []ast.Expr{
+														0: &ast.BinaryExpr{
+															X: &ast.BasicLit{
+																Kind:  token.STRING,
+																Value: "\"signal: \"",
+															},
+															Op: token.ADD,
+															Y: &ast.CallExpr{
+																Fun: &ast.SelectorExpr{
+																	X:   ast.NewIdent("s"),
+																	Sel: ast.NewIdent("String"),
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+									1: &ast.CommClause{
+										Comm: &ast.AssignStmt{
+											Lhs: []ast.Expr{
+												ast.NewIdent("err"),
+											},
+											Tok: token.ASSIGN,
+											Rhs: []ast.Expr{
+												&ast.UnaryExpr{
+													Op: token.ARROW,
+													X: &ast.CallExpr{
+														Fun: &ast.SelectorExpr{
+															X:   ast.NewIdent("serv"),
+															Sel: ast.NewIdent("Notify"),
+														},
+													},
+												},
+											},
+										},
+										Body: []ast.Stmt{
+											&ast.ExprStmt{
+												X: &ast.CallExpr{
+													Fun: &ast.SelectorExpr{
+														X:   ast.NewIdent("log"),
+														Sel: ast.NewIdent("Printf"),
+													},
+													Args: []ast.Expr{
+														&ast.BasicLit{
+															Kind:  token.STRING,
+															Value: "\"Notify from http server\"",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						8: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								ast.NewIdent("err"),
+							},
+							Tok: token.ASSIGN,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: &ast.SelectorExpr{
+										X:   ast.NewIdent("serv"),
+										Sel: ast.NewIdent("Shutdown"),
+									},
+								},
+							},
+						},
+						9: &ast.IfStmt{
+							Cond: &ast.BinaryExpr{
+								X:  ast.NewIdent("err"),
+								Op: token.NEQ,
+								Y:  ast.NewIdent("nil"),
+							},
+							Body: &ast.BlockStmt{
+								List: []ast.Stmt{
+									&ast.ExprStmt{
+										X: &ast.CallExpr{
+											Fun: &ast.SelectorExpr{
+												X:   ast.NewIdent("log"),
+												Sel: ast.NewIdent("Printf"),
+											},
+											Args: []ast.Expr{
+												&ast.BasicLit{
+													Kind:  token.STRING,
+													Value: "\"Http server shutdown\"",
+												},
+											},
+										},
 									},
 								},
 							},
@@ -50,14 +458,19 @@ func createAppAST() *ast.File {
 }
 
 // GenApp - Генерация App.go
-func GenApp() error {
-	f := createAppAST()
+func GenApp(moduleName string) error {
+	f := createAppAST(moduleName)
 	fset := token.NewFileSet()
 	file, err := os.Create("internal/app/app.go")
 	if err != nil {
 		return fmt.Errorf("error creating app.go file: %w", err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Printf("error in closing file %s", file.Name())
+		}
+	}(file)
 	err = printer.Fprint(file, fset, f)
 	if err != nil {
 		return fmt.Errorf("error in generate app.go: %w", err)
--- internal/gen/base/main_gen.go
+++ internal/gen/base/main_gen.go
@@ -8,37 +8,89 @@ import (
 	"os"
 )
 
-func createMainAST(nameModule string) *ast.File {
+func createMainAST(moduleName string) *ast.File {
 	return &ast.File{
 		Name: ast.NewIdent("main"),
 		Decls: []ast.Decl{
-			&ast.GenDecl{
+			0: &ast.GenDecl{
 				Tok: token.IMPORT,
 				Specs: []ast.Spec{
-					&ast.ImportSpec{
+					0: &ast.ImportSpec{
 						Path: &ast.BasicLit{
 							Kind:  token.STRING,
-							Value: "\"" + nameModule + "/internal/" + "app\"",
+							Value: "\"log\"",
+						},
+					},
+					1: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"" + moduleName + "/configs\"",
+						},
+					},
+					2: &ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"" + moduleName + "/internal/app\"",
 						},
 					},
 				},
 			},
-			&ast.FuncDecl{
+			1: &ast.FuncDecl{
 				Name: ast.NewIdent("main"),
 				Type: &ast.FuncType{
-					Params: &ast.FieldList{
-						Opening: 10,
-						Closing: 11,
-					},
+					Params: nil,
 				},
 				Body: &ast.BlockStmt{
-					//Lbrace: 13,
 					List: []ast.Stmt{
-						&ast.ExprStmt{
+						0: &ast.AssignStmt{
+							Lhs: []ast.Expr{
+								0: ast.NewIdent("cfg"),
+								1: ast.NewIdent("err"),
+							},
+							Tok: token.DEFINE,
+							Rhs: []ast.Expr{
+								&ast.CallExpr{
+									Fun: &ast.SelectorExpr{
+										X:   ast.NewIdent("configs"),
+										Sel: ast.NewIdent("NewConfig"),
+									},
+								},
+							},
+						},
+						1: &ast.IfStmt{
+							Cond: &ast.BinaryExpr{
+								X:  ast.NewIdent("err"),
+								Op: token.NEQ,
+								Y:  ast.NewIdent("nil"),
+							},
+							Body: &ast.BlockStmt{
+								List: []ast.Stmt{
+									&ast.ExprStmt{
+										X: &ast.CallExpr{
+											Fun: &ast.SelectorExpr{
+												X:   ast.NewIdent("log"),
+												Sel: ast.NewIdent("Fatalf"),
+											},
+											Args: []ast.Expr{
+												0: &ast.BasicLit{
+													Kind:  token.STRING,
+													Value: "\"Error in parse config: %s\\n\"",
+												},
+												1: ast.NewIdent("err"),
+											},
+										},
+									},
+								},
+							},
+						},
+						2: &ast.ExprStmt{
 							X: &ast.CallExpr{
 								Fun: &ast.SelectorExpr{
 									X:   ast.NewIdent("app"),
 									Sel: ast.NewIdent("Run"),
+								},
+								Args: []ast.Expr{
+									ast.NewIdent("cfg"),
 								},
 							},
 						},
@@ -50,8 +102,8 @@ func createMainAST(nameModule string) *ast.File {
 }
 
 // GenMain - генерация main.go
-func GenMain(nameModule string) error {
-	f := createMainAST(nameModule)
+func GenMain(moduleName string) error {
+	f := createMainAST(moduleName)
 	fset := token.NewFileSet()
 
 	file, err := os.Create("cmd/main/main.go")
