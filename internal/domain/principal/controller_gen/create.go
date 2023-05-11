95704fc03419bfd7b1c6116d3773613a78f0907d
romeros69 <jefopc@gmail.com>
2023-05-11 18:08:17 +0300

    feat: gen http create

--- internal/domain/principal/controller_gen/create.go
+++ internal/domain/principal/controller_gen/create.go
@@ -20,7 +20,7 @@ func (cg *ControllerHTTPGenerator) genCreateControllerAST() *ast.FuncDecl {
 				},
 			},
 		},
-		Name: ast.NewIdent("get" + cg.entAST.Name + "List"),
+		Name: ast.NewIdent("create" + cg.entAST.Name),
 		Type: &ast.FuncType{
 			Params: &ast.FieldList{
 				List: []*ast.Field{
@@ -31,7 +31,7 @@ func (cg *ControllerHTTPGenerator) genCreateControllerAST() *ast.FuncDecl {
 						Type: &ast.StarExpr{
 							X: &ast.SelectorExpr{
 								X:   ast.NewIdent("gin"),
-								Sel: ast.NewIdent("context"),
+								Sel: ast.NewIdent("Context"),
 							},
 						},
 					},
@@ -40,11 +40,79 @@ func (cg *ControllerHTTPGenerator) genCreateControllerAST() *ast.FuncDecl {
 		},
 		Body: &ast.BlockStmt{
 			List: []ast.Stmt{
-				0: cg.genCallServiceCreate(),
-				1: cg.genCheckCallServiceCreate(),
-				2: cg.genDeclResponseCreate(),
-				3: cg.genCheckNilResponseCreate(),
-				4: cg.genSendResponseCreate(),
+				0: cg.genDefineRequestCreate(),
+				1: cg.genParseRequestCreate(),
+				2: cg.genCallServiceCreate(),
+				3: cg.genCheckCallServiceCreate(),
+				4: cg.genHeaderCreate(),
+				5: cg.genSendResponseCreate(),
+			},
+		},
+	}
+}
+
+func (cg *ControllerHTTPGenerator) genDefineRequestCreate() *ast.AssignStmt {
+	return &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			ast.NewIdent("req"),
+		},
+		Tok: token.DEFINE,
+		Rhs: []ast.Expr{
+			&ast.CallExpr{
+				Fun: ast.NewIdent("new"),
+				Args: []ast.Expr{
+					ast.NewIdent(strings.ToLower(cg.entAST.Name) + "Request"),
+				},
+			},
+		},
+	}
+}
+
+func (cg *ControllerHTTPGenerator) genParseRequestCreate() *ast.IfStmt {
+	return &ast.IfStmt{
+		Init: &ast.AssignStmt{
+			Lhs: []ast.Expr{
+				ast.NewIdent("err"),
+			},
+			Tok: token.DEFINE,
+			Rhs: []ast.Expr{
+				&ast.CallExpr{
+					Fun: &ast.SelectorExpr{
+						X:   ast.NewIdent("c"),
+						Sel: ast.NewIdent("ShouldBindJSON"),
+					},
+					Args: []ast.Expr{
+						ast.NewIdent("req"),
+					},
+				},
+			},
+		},
+		Cond: &ast.BinaryExpr{
+			X:  ast.NewIdent("err"),
+			Op: token.NEQ,
+			Y:  ast.NewIdent("nil"),
+		},
+		Body: &ast.BlockStmt{
+			List: []ast.Stmt{
+				0: &ast.ExprStmt{
+					X: &ast.CallExpr{
+						Fun: ast.NewIdent("errorResponse"),
+						Args: []ast.Expr{
+							0: ast.NewIdent("c"),
+							1: &ast.SelectorExpr{
+								X:   ast.NewIdent("http"),
+								Sel: ast.NewIdent("StatusBadRequest"),
+							},
+							2: &ast.CallExpr{
+								Fun: &ast.SelectorExpr{
+									X:   ast.NewIdent("err"),
+									Sel: ast.NewIdent("Error"),
+								},
+							},
+						},
+					},
+				},
+				1: &ast.ReturnStmt{},
 			},
 		},
 	}
@@ -53,7 +121,7 @@ func (cg *ControllerHTTPGenerator) genCreateControllerAST() *ast.FuncDecl {
 func (cg *ControllerHTTPGenerator) genCallServiceCreate() *ast.AssignStmt {
 	return &ast.AssignStmt{
 		Lhs: []ast.Expr{
-			0: ast.NewIdent(cg.entAST.Name + "List"),
+			0: ast.NewIdent(strings.ToLower(cg.entAST.Fields[0].Name)),
 			1: ast.NewIdent("err"),
 		},
 		Tok: token.DEFINE,
@@ -64,10 +132,10 @@ func (cg *ControllerHTTPGenerator) genCallServiceCreate() *ast.AssignStmt {
 						X:   ast.NewIdent(string(strings.ToLower(cg.entAST.Name)[0]) + "r"),
 						Sel: ast.NewIdent(string(strings.ToLower(cg.entAST.Name)[0])),
 					},
-					Sel: ast.NewIdent("Get" + cg.entAST.Name + "List"),
+					Sel: ast.NewIdent("Create" + cg.entAST.Name),
 				},
 				Args: []ast.Expr{
-					&ast.CallExpr{
+					0: &ast.CallExpr{
 						Fun: &ast.SelectorExpr{
 							X: &ast.SelectorExpr{
 								X:   ast.NewIdent("c"),
@@ -75,6 +143,25 @@ func (cg *ControllerHTTPGenerator) genCallServiceCreate() *ast.AssignStmt {
 							},
 							Sel: ast.NewIdent("Context"),
 						},
+					},
+					1: &ast.CompositeLit{
+						Type: &ast.SelectorExpr{
+							X:   ast.NewIdent("entity"),
+							Sel: ast.NewIdent(cg.entAST.Name),
+						},
+						Elts: func() []ast.Expr {
+							res := make([]ast.Expr, 0)
+							for i := 1; i < len(cg.entAST.Fields); i++ {
+								res = append(res, &ast.KeyValueExpr{
+									Key: ast.NewIdent(cg.entAST.Fields[i].Name),
+									Value: &ast.SelectorExpr{
+										X:   ast.NewIdent("req"),
+										Sel: ast.NewIdent(cg.entAST.Fields[i].Name),
+									},
+								})
+							}
+							return res
+						}(),
 					},
 				},
 			},
@@ -115,68 +202,35 @@ func (cg *ControllerHTTPGenerator) genCheckCallServiceCreate() *ast.IfStmt {
 	}
 }
 
-func (cg *ControllerHTTPGenerator) genDeclResponseCreate() *ast.DeclStmt {
-	return &ast.DeclStmt{
-		Decl: &ast.GenDecl{
-			Tok: token.VAR,
-			Specs: []ast.Spec{
-				&ast.ValueSpec{
-					Names: []*ast.Ident{
-						ast.NewIdent("req"),
+func (cg *ControllerHTTPGenerator) genHeaderCreate() *ast.ExprStmt {
+	return &ast.ExprStmt{
+		X: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X:   ast.NewIdent("c"),
+				Sel: ast.NewIdent("Header"),
+			},
+			Args: []ast.Expr{
+				0: &ast.BasicLit{
+					Kind:  token.STRING,
+					Value: "\"Location\"",
+				},
+				1: &ast.CallExpr{
+					Fun: &ast.SelectorExpr{
+						X:   ast.NewIdent("fmt"),
+						Sel: ast.NewIdent("Sprintf"),
 					},
-					Type: &ast.ArrayType{
-						Elt: &ast.SelectorExpr{
-							X:   ast.NewIdent("entity"),
-							Sel: ast.NewIdent(cg.entAST.Name),
+					Args: []ast.Expr{
+						0: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: "\"/api/v1/" + strings.ToLower(cg.entAST.Name) + "/%s\"",
 						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func (cg *ControllerHTTPGenerator) genCheckNilResponseCreate() *ast.IfStmt {
-	return &ast.IfStmt{
-		Cond: &ast.BinaryExpr{
-			X:  ast.NewIdent(strings.ToLower(cg.entAST.Name) + "List"),
-			Op: token.NEQ,
-			Y:  ast.NewIdent("nil"),
-		},
-		Body: &ast.BlockStmt{
-			List: []ast.Stmt{
-				&ast.AssignStmt{
-					Lhs: []ast.Expr{
-						ast.NewIdent("req"),
-					},
-					Tok: token.ASSIGN,
-					Rhs: []ast.Expr{
-						ast.NewIdent(strings.ToLower(cg.entAST.Name) + "List"),
-					},
-				},
-			},
-		},
-		Else: &ast.BlockStmt{
-			List: []ast.Stmt{
-				&ast.AssignStmt{
-					Lhs: []ast.Expr{
-						ast.NewIdent("req"),
-					},
-					Tok: token.ASSIGN,
-					Rhs: []ast.Expr{
-						&ast.CallExpr{
-							Fun: ast.NewIdent("make"),
+						1: &ast.CallExpr{
+							Fun: &ast.SelectorExpr{
+								X:   ast.NewIdent("strconv"),
+								Sel: ast.NewIdent("Itoa"),
+							},
 							Args: []ast.Expr{
-								0: &ast.ArrayType{
-									Elt: &ast.SelectorExpr{
-										X:   ast.NewIdent("entity"),
-										Sel: ast.NewIdent(cg.entAST.Name),
-									},
-								},
-								1: &ast.BasicLit{
-									Kind:  token.INT,
-									Value: "0",
-								},
+								ast.NewIdent(strings.ToLower(cg.entAST.Fields[0].Name)),
 							},
 						},
 					},
@@ -196,9 +250,9 @@ func (cg *ControllerHTTPGenerator) genSendResponseCreate() *ast.ExprStmt {
 			Args: []ast.Expr{
 				0: &ast.SelectorExpr{
 					X:   ast.NewIdent("http"),
-					Sel: ast.NewIdent("StatusOK"),
+					Sel: ast.NewIdent("StatusCreated"),
 				},
-				1: ast.NewIdent("req"),
+				1: ast.NewIdent("nil"),
 			},
 		},
 	}
